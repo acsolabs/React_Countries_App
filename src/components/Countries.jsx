f321039acfc453cbd215d26ebfc8e95fbf7fc893
acsolabs <sofian.achbabi@gmail.com>
2026-04-15 16:51:32 +0200

    ✨ HOME > Countries : add filter by continent

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -5,6 +5,8 @@ import Card from "./Card";
 const Countries = () => {
   const [data, setData] = useState([]);
   const [rangeValue, setRangeValue] = useState("36");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+  const [selectedRadio, setSelectedRadio] = useState("");
 
   const handleChange = (e) => {
     setRangeValue(e.target.value);
@@ -13,7 +15,7 @@ const Countries = () => {
   useEffect(() => {
     axios
       .get(
-        "https://restcountries.com/v3.1/all?fields=name,flags,population,capital",
+        "https://restcountries.com/v3.1/all?fields=name,flags,population,capital,region",
       )
       .then((res) => setData(res.data));
   }, []);
@@ -28,11 +30,25 @@ const Countries = () => {
           onChange={handleChange}
           defaultValue={rangeValue}
         />
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              name="continentRadio"
+              id={continent}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
       </ul>
       <ul>
-        {data.slice(0, rangeValue).map((country, index) => (
-          <Card key={index} country={country} />
-        ))}
+        {data
+          .filter((country) => country.region.includes(selectedRadio))
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Card key={index} country={country} />
+          ))}
       </ul>
     </div>
   );
