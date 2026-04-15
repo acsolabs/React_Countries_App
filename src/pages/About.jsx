53bbc6138a248214a9eb3c44b8bb36a88a691bda
acsolabs <sofian.achbabi@gmail.com>
2026-04-15 15:46:00 +0200

    ✨ About > create About page Component

---
+++ src/pages/About.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
+        mollitia omnis labore sequi autem neque facilis magnam quos vel veniam!
+        Reprehenderit repudiandae eaque eius in alias corporis mollitia
+        dignissimos nisi voluptas nemo aliquam dolores maxime et quas velit
+        architecto ab, labore modi, tenetur ipsum enim quam commodi officiis!
+        Blanditiis nostrum aspernatur facilis cupiditate pariatur exercitationem
+        commodi nobis voluptatem ea, provident maxime deleniti recusandae beatae
+        totam suscipit, esse sint, quas porro illo iste vel similique eum
+        aliquam ad! Vero quisquam facere ducimus nisi aliquam harum sint,
+        aspernatur quae dolore ratione sapiente. Fugit obcaecati ipsum vel
+        aliquam velit quae, nulla harum nobis!
+      </p>
+    </div>
+  );
+};
+
+export default About;
