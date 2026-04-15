c8608c2481cd2555e455bb48dc42261ca92f0fdf
acsolabs <sofian.achbabi@gmail.com>
2026-04-15 15:43:22 +0200

    ✨ add routes Home & About

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="*" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
